84c366fa840ec473ac3298eabac3f1812bbf85dd
The Crowndite <thecrownofficialdite@gmail.com>
2025-12-09 17:32:19 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://encrypt

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn1.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
